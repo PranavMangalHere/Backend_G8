242b6b71b7c5dfbf52e87f1ec148d8af1e49c175
pranavmanga <123859934+pranavmanga@users.noreply.github.com>
2024-08-29 22:12:14 +0530

    29_08_2024

---
+++ new.js
@@ -0,0 +1,7 @@
+db.user2.insertMany([
+    { name: "Alice", age: 25, email: "alice@example.com" },
+    { name: "Bob", age: 30, email: "bob@example.com" },
+    { name: "Charlie", age: 28, email: "charlie@example.com" },
+    { name: "David", age: 35, email: "david@example.com" },
+    { name: "Eve", age: 22, email: "eve@example.com" }
+])
